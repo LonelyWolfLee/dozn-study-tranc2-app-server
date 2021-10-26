7b6c1df253794bdfba0eaa0f8e3618456e4205e6
hoan.lee <lonelywolf.lee@gmail.com>
2021-10-26 13:27:01 +0900

    Add comment to explain apis

--- main.go
+++ main.go
@@ -21,6 +21,14 @@ func main() {
 	app.Use(logger.New())
 
 	// Routes
+	// 1. Registration (User)
+	// 2. Login (User)
+	// 3. Auto login (User)
+	// 4. Create bank account (User, Account)
+	// 5. Get bank accounts (User, Account)
+	// 6. Transaction list for one bank account (User, Account, Transaction)
+	// 7. Money tranfer (User, Account, Transaction)
+	// 8. Deposit (User, Account, Transaction)
 	app.Get("/", hello)
 
 	// Start server
