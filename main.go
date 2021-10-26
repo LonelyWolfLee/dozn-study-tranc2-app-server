3baaa6ed8106dbd67c87cc4151b8f772f15a278a
이용환 <loenlywolf.lee@gmail.com>
2021-10-26 13:59:25 +0900

    Refactoring trace

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"dozn/app-server/utils"
+	"dozn/app-server/logging"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -9,10 +9,10 @@ import (
 
 func main() {
 
-	utils.LOG.Trace.Println("Starting the application...")
-	utils.LOG.Info.Println("Something noteworthy happened...")
-	utils.LOG.Warn.Println("There is something you should know about...")
-	utils.LOG.Error.Println("Something went wrong...")
+	logging.Trace("Starting the application...")
+	logging.Info("Something noteworthy happened...")
+	logging.Warn("There is something you should know about...")
+	logging.Error("Something went wrong...")
 
 	// Echo instance
 	app := fiber.New()
